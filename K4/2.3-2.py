16fec4bbe8b59cf3f56d88cb36e76021e5d70e40
King Star <mcxin.y@gmail.com>
2023-10-27 15:02:39 +0800

    Remove additonal files.

--- K4/2.3-2.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def fib( n ):
-    return (1 / 5 ** 0.5) * (((1 + 5 ** 0.5) / 2) ** i - ((1 - 5 ** 0.5) / 2) ** i)
-
-t = time.time()
-for i in range(1, 51):
-    print('F(' + str(i) + ')=' + str(int(fib(i))))
-print(f'Time run: {time.time() - t:.4f}s')
